45e6282b4897fad28fbb398a306c31c87169afac
areif-dev <ajreifsnyder@protonmail.com>
2024-10-31 14:21:55 -0400

    AbcProduct: Add getters and builder struct

--- src/models.rs
+++ src/models.rs
@@ -82,6 +82,7 @@ impl TryFrom<&str> for Upc {
 }
 
 pub struct AbcProduct {
+    abc_product_id: i64,
     sku: String,
     desc: String,
     upc: Upc,
@@ -90,3 +91,117 @@ pub struct AbcProduct {
     stock: f64,
 }
 
+impl AbcProduct {
+    pub fn abc_product_id(&self) -> i64 {
+        self.abc_product_id
+    }
+
+    pub fn sku(&self) -> String {
+        self.sku.clone()
+    }
+
+    pub fn desc(&self) -> String {
+        self.desc.clone()
+    }
+
+    pub fn upc(&self) -> Upc {
+        self.upc.to_owned()
+    }
+
+    pub fn list(&self) -> i64 {
+        self.list
+    }
+
+    pub fn cost(&self) -> i64 {
+        self.cost
+    }
+
+    pub fn stock(&self) -> f64 {
+        self.stock
+    }
+}
+
+pub struct AbcProductBuilder {
+    abc_product_id: Option<i64>,
+    sku: Option<String>,
+    desc: Option<String>,
+    upc: Option<Upc>,
+    list: Option<i64>,
+    cost: Option<i64>,
+    stock: Option<f64>,
+}
+
+impl AbcProductBuilder {
+    pub fn new() -> Self {
+        AbcProductBuilder {
+            abc_product_id: None,
+            sku: None,
+            desc: None,
+            upc: None,
+            list: None,
+            cost: None,
+            stock: None,
+        }
+    }
+
+    pub fn with_abc_product_id(self, id: i64) -> Self {
+        AbcProductBuilder {
+            abc_product_id: Some(id),
+            ..self
+        }
+    }
+
+    pub fn with_sku(self, sku: &str) -> Self {
+        AbcProductBuilder {
+            sku: Some(sku.to_string()),
+            ..self
+        }
+    }
+
+    pub fn with_desc(self, desc: &str) -> Self {
+        AbcProductBuilder {
+            desc: Some(desc.to_string()),
+            ..self
+        }
+    }
+
+    pub fn with_upc(self, upc: Upc) -> Self {
+        AbcProductBuilder {
+            upc: Some(upc),
+            ..self
+        }
+    }
+
+    pub fn with_list(self, list: i64) -> Self {
+        AbcProductBuilder {
+            list: Some(list),
+            ..self
+        }
+    }
+
+    pub fn with_cost(self, cost: i64) -> Self {
+        AbcProductBuilder {
+            cost: Some(cost),
+            ..self
+        }
+    }
+
+    pub fn with_stock(self, stock: f64) -> Self {
+        AbcProductBuilder {
+            stock: Some(stock),
+            ..self
+        }
+    }
+
+    pub fn build(self) -> Option<AbcProduct> {
+        Some(AbcProduct {
+            abc_product_id: self.abc_product_id?,
+            sku: self.sku.clone()?,
+            desc: self.desc.clone()?,
+            upc: self.upc?,
+            list: self.list?,
+            cost: self.cost?,
+            stock: self.stock?,
+        })
+    }
+}
